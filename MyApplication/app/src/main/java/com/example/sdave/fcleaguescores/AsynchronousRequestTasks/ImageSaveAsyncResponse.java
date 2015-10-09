6b7eeb376cbf8fdd67125b0bd244fb61dba81041
Sagar Dave <davesagar2012@gmail.com>
2015-10-09 10:51:14 -0400

    rename app and directory

--- MyApplication/app/src/main/java/com/example/sdave/fcleaguescores/AsynchronousRequestTasks/ImageSaveAsyncResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.sdave.fcleaguescores.AsynchronousRequestTasks;
-
-/**
- * Created by sdave on 4/21/2015.
- */
-public interface ImageSaveAsyncResponse {
-    void processFinish(String response);
-
-}
